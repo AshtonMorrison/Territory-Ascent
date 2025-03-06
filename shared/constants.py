16bca68f3d33cdb69c9cd0f0a03c2705b924085a
Ashton Linux <ironslamhead@gmail.com>
2025-03-06 11:14:33 -0800

    Changed game resolution to 640x360, good for pixel game. We can scale later

--- shared/constants.py
+++ shared/constants.py
@@ -3,6 +3,6 @@ PORT = 5555
 HOST = "localhost" # Will change later when server side gets made
 
 # Game settings
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 360
 FPS = 60
