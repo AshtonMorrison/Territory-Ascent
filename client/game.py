1f5b00327ddc1cc1d2930557f6bd286fd684442e
Ashton Linux <ironslamhead@gmail.com>
2025-03-06 11:15:40 -0800

    Added tab title, FPS limit, and white background

--- client/game.py
+++ client/game.py
@@ -4,7 +4,13 @@ from shared import constants
 class GameClient:
     def __init__(self):
         pygame.init()
+
+        # Display
         self.screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+        pygame.display.set_caption("371 Multiplayer Game")
+
+        # Clock for FPS
+        self.clock = pygame.time.Clock()
 
     def run(self):
         running = True
@@ -12,7 +18,15 @@ class GameClient:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+            
+            # Everything gets done to the back buffer
+            # Drawing
+            self.screen.fill((255, 255, 255))
 
+            # FPS Limit
+            self.clock.tick(constants.FPS)
+
+            # Flip the back buffer to the front
             pygame.display.flip()
 
         pygame.quit()
