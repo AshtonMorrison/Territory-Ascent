2bda5adcd217638bfd7f2bcb90f69cbce1c5b354
Ashton Linux <ironslamhead@gmail.com>
2025-03-06 10:57:04 -0800

    Now able to run the game with a screen. To run, use the following command in 371-multi directory: python -m client.game

--- client/game.py
+++ client/game.py
@@ -1,12 +1,21 @@
 import pygame
-
+from shared import constants
 
 class GameClient:
     def __init__(self):
         pygame.init()
+        self.screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
 
     def run(self):
-        pass
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+            pygame.display.flip()
+
+        pygame.quit()
 
 
 if __name__ == "__main__":
--- shared/constants.py
+++ shared/constants.py
@@ -1,6 +1,6 @@
 # Network settings
 PORT = 5555
-HOST = "localhost"
+HOST = "localhost" # Will change later when server side gets made
 
 # Game settings
 SCREEN_WIDTH = 800
