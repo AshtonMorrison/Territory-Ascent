b31a16922a97e92f0742436d92faad01fe6d7754
Ashton Linux <ironslamhead@gmail.com>
2025-03-17 16:08:12 -0700

    moved to server side

---
+++ server/tile.py
@@ -0,0 +1,44 @@
+import pygame
+import os
+from shared import constants
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, x, y, width, height, image_integer, sub_group=""):
+        super().__init__()
+        
+        if image_integer == 1:
+            self.image = pygame.Surface([width, height])
+            ground_color = (170, 120, 80)
+            border_color = (150, 100, 60)
+
+            self.image.fill(border_color)
+            pygame.draw.rect(
+                self.image, ground_color, [0, height // 2, width, height // 2]
+            )
+
+            sub_group.add(self)
+
+        if image_integer == 2:
+            self.image = pygame.Surface([width, height])
+            platform_color = (
+                constants.DEFAULT_PLATFORM_COLOR
+            )  # Use default platform color for now; will have to adjust for coloured occupation later
+            border_color = (
+                platform_color[0] - 30,
+                platform_color[1] - 30,
+                platform_color[2] - 30,
+            )
+
+            self.image.fill(border_color)
+            pygame.draw.rect(
+                self.image, platform_color, [0, height // 2, width, height // 2]
+            )
+
+            sub_group.add(self)
+
+        # Get rects and positions
+        self.X = x
+        self.Y = y
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
