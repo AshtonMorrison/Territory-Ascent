07ec143558892f68ac67e1269dc7fac367809eac
Ashton Linux <ironslamhead@gmail.com>
2025-03-17 16:08:22 -0700

    began work on server

--- server/server.py
+++ server/server.py
@@ -1,9 +1,118 @@
+import socket
+import json
+import threading
+import pygame
+from shared import constants
+from tile import Tile
+from player import Player
+
+
 class GameServer:
-    def __init__(self):
+    def __init__(self, host="0.0.0.0"):
+        self.host = host
+        self.port = constants.PORT
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.clients = {} # Maps client address to player objects
+        self.tile_groups = {
+            "ground": pygame.sprite.Group(), # Used for Collision
+            "platform": pygame.sprite.Group(), # Used for Collision
+        }
+        self.lock = threading.Lock()
+
+        self.tile_size = constants.TILE_SIZE
+        self.tile_dict = {} # used to access tiles by (x, y) coordinates. MIGHT NOT NEED DEPENDING ON LATER IMPLEMENTATION
+        self.tile_data = [] # used for initial sending of tile map to clients
+
+        # Tilemap layout (0: empty, 1: ground, 2: platform)
+        self.tile_map = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+]
+        self.create_tile_map()
+
+    def create_tile_map(self):
+        for row in range(len(self.tile_map)): 
+            for col in range(len(self.tile_map[row])):
+                x = col * self.tile_size
+                y = row * self.tile_size
+
+                # Ground
+                if self.tile_map[row][col] == 1:
+                    self.tile_dict[(x, y)] = Tile(x, y, self.tile_size, self.tile_size, 1, self.tile_groups["ground"])
+                    self.tile_data.append({"x": x, "y": y, "type": 1})
+
+                # Platform
+                elif self.tile_map[row][col] == 2:
+                    self.tile_dict[{x, y}] = Tile(x, y, self.tile_size, self.tile_size, 2, self.tile_groups["platform"])
+                    self.tile_data.append({"x": x, "y": y, "type": 2})
+
+    def handle_client(self, conn, addr):
+        print(f"New connection: {addr}")
+        player = Player((255, 0, 0), 100, 100, self.tile_size, self.tile_size)
+        self.clients[addr] = player
+        
+        # Send initial tile map
+        conn.sendall(json.dumps({"type": "MAP", "TileMap": self.tile_data}).encode())
+        
+        try:
+            while True:
+                data = conn.recv(1024).decode()
+                if not data:
+                    break
+
+                # PUT HERE:
+                # Client -> Server code, input managing and player updating
+
+        except Exception as e:
+            print(f"Error with client {addr}: {e}")
+        finally:
+            conn.close()
+            with self.lock:
+                del self.clients[addr]
+            print(f"Client {addr} disconnected.")
+
+    def get_player_state(self):
+        return [{"x": p.position.x, "y": p.position.y, "color": p.color} for p in self.clients.values()]
+    
+    def get_map_state(self):
+        # TO BE ADDED FOR WHEN TILES ACTUALLY CHANGE
+        # So, [{"x": t.x, "y": t.y, "color": t.color} for t in self.tiles] or something similar
         pass
 
-    def start(self):
+
+    def broadcast(self):
         pass
+    
+    def start(self):
+        self.server.bind((self.host, self.port))
+        self.server.listen()
+        print(f"Server listening on {self.host}:{self.port}")
+
+
+        while True:
+            conn, addr = self.server.accept()
+            threading.Thread(target=self.handle_client, args=(conn, addr)).start()
 
 
 if __name__ == "__main__":
